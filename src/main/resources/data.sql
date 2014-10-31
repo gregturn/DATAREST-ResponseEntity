c69b347c66b530f84ebba6fed7a0f0a3679c13d4
Rob Winch <rwinch@gopivotal.com>
2014-10-31 17:26:38 -0500

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into user(id,username) values (1,'username');
---
+++ src/test/java/demo/ApplicationTests.java
@@ -0,0 +1,46 @@
+package demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.AssertThrows;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTests {
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    public void sameJson() throws Exception {
+        String authenticate = mockMvc
+                .perform(get("/authenticate"))
+                .andReturn().getResponse().getContentAsString();
+        mockMvc
+            .perform(get("/users/1"))
+            .andExpect(content().string(authenticate));
+    }
+
+}
