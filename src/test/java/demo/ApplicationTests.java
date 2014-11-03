417b0140f7d77f62e9212c7acca0baebc11f533f
Greg Turnquist <gturnquist@gopivotal.com>
2014-11-03 08:51:23 -0600

    Explicitly specify accept header for non-SDR end points
    * SDR was fixed so that non-SDR endpoints don't default to HAL. You were comparing results of a non-SDR endpoint against an SDR one, and it broke. Fixed that.
    * As a consequence, upgraded to latest stable release of Spring Boot

--- src/test/java/demo/ApplicationTests.java
+++ src/test/java/demo/ApplicationTests.java
@@ -1,20 +1,20 @@
 package demo;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.AssertThrows;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -35,9 +35,12 @@ public class ApplicationTests {
 
     @Test
     public void sameJson() throws Exception {
+        // Ensure that /authenticate returns application/hal+json like the
+        // Spring Data REST call will.
         String authenticate = mockMvc
-                .perform(get("/authenticate"))
+                .perform(get("/authenticate").accept(MediaTypes.HAL_JSON))
                 .andReturn().getResponse().getContentAsString();
+
         mockMvc
             .perform(get("/users/1"))
             .andExpect(content().string(authenticate));
